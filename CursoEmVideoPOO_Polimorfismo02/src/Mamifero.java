95ba5bfe3f3da59887f73407e487627a78813f18
dayclar <claradayanamcm07@gmail.com>
2022-12-13 20:00:35 -0300

    POO

--- CursoEmVideoPOO_Polimorfismo02/src/Mamifero.java
+++ CursoEmVideoPOO_Polimorfismo02/src/Mamifero.java
@@ -2,6 +2,14 @@
 public class Mamifero extends Animal {
 
 	protected String corPelo;
+	
+	public String getCorPelo() {
+		return corPelo;
+	}
+
+	public void setCorPelo(String corPelo) {
+		this.corPelo = corPelo;
+	}
 
 	@Override
 	public void emitirSom() {
